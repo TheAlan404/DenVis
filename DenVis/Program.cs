f68a5fad77d0afee2085ab7b2012b22bf7660874
TheAlan404 <43997085+TheAlan404@users.noreply.github.com>
2022-01-12 21:49:19 +0300

    yes

--- DenVis/Program.cs
+++ DenVis/Program.cs
@@ -7,6 +7,18 @@ using CSCore.Streams;
 using CSCore;
 using CSCore.Streams.Effects;
 using System.Linq;
+using CSCore.DSP;
+using CSCore.Utils;
+using System.Text;
+using System.Threading;
+
+/*
+ 
+ /// TW: SHITCODE AHEAD
+ 
+im sorry -dennis
+ 
+ */
 
 namespace DenVis
 {
@@ -16,17 +28,26 @@ namespace DenVis
 		public static GraphicsWindow graphicsWindow;
 		public static int screenW = Utils.GetDisplay().Item1;
 		public static int screenH = Utils.GetDisplay().Item2;
-		public static int heightMultiplier = 1;
-		public static int pointDistance;
+		public static float heightMultiplier = 100; // Program: im sensitive uwu
+		public static bool IsOnTopOfTaskbar = true;
+		public static float yOffset = 220 + (IsOnTopOfTaskbar ? 38 : 0);
+		public static float pointDistance = ((int)fftSize) / screenW;
+		public static int maxHeight = 500;
+
+		
 
 		// Graphics
 		public static SolidBrush Brush;
-
-		// Capture
+		public static Font Font;
+		
+		// Sound
 		public static WasapiLoopbackCapture soundIn;
+		public const FftSize fftSize = FftSize.Fft4096;
+		public static FftProvider fftProvider;
 
 		// Shared
 		public static float[] soundData;
+		public static bool _initialized = false;
 
 		public static void Main()
 		{
@@ -34,9 +55,9 @@ namespace DenVis
 
 			GameOverlay.TimerService.EnableHighPrecisionTimers();
 			SetupGraphics();
-			SetupCapture();
 
 			graphicsWindow.Create();
+			SetupCapture();
 			Console.WriteLine("Started");
 			graphicsWindow.Join();
 		}
@@ -52,7 +73,7 @@ namespace DenVis
 
 			graphicsWindow = new GraphicsWindow(0, 0, screenW, screenH, gfx)
 			{
-				FPS = 10,
+				FPS = 30,
 				IsTopmost = true,
 				IsVisible = true
 			};
@@ -62,6 +83,7 @@ namespace DenVis
 				var gfx = e.Graphics;
 				if (e.RecreateResources) Brush.Dispose();
 				Brush = gfx.CreateSolidBrush(0, 0, 255, 128);
+				Font = gfx.CreateFont("Arial", 10);
 				Console.WriteLine($"SetupGraphics called");
 			};
 
@@ -78,26 +100,115 @@ namespace DenVis
 
 		public static void Render(object _, DrawGraphicsEventArgs e)
 		{
+			if (!_initialized) return;
+
 			var gfx = e.Graphics;
 
 			gfx.ClearScene();
 
-			gfx.DrawBox2D(Brush, Brush, new Rectangle(0, 0, 100, 100), 1);
+			float[] data = new float[(int)fftSize];
 
-			if (soundData == null || soundData.Length == 0) return;
+			fftProvider.GetFftData(data);
 
-			float posY = 0;
-			float posX = -pointDistance;
-			foreach(float b in soundData)
+			float maxValue = data.Max();
+			if (maxValue < 0.0001) Array.Fill(data, 0f);
+
+			data = Normalize(data);
+
+			StringBuilder sb = new StringBuilder();
+
+			
+
+			float previousValue = 0f;
+			float xPosition = pointDistance;
+			foreach(float value in data)
 			{
+				gfx.DrawLine(Brush,
+					// start x
+					xPosition - pointDistance,
+					// start y
+					ValueToY(previousValue),
+					// end x
+					xPosition,
+					// end y
+					ValueToY(value),
+
+					// stroke
+					5
+				);
+
+				// sorry for my shitcode
+				//(float r, float g, float b) nextColor = Utils.AddHueToColor(Brush.Color.R, Brush.Color.G, Brush.Color.B);
+				//Brush.Color = new Color(nextColor.r, nextColor.g, nextColor.b);
+				//var c = System.Drawing.Color.FromArgb(255, ConvColor(Brush.Color.R), ConvColor(Brush.Color.G), ConvColor(Brush.Color.B));
+				//var nextColor = ColorScale.ColorFromHSL((c.GetHue() + 1) % 360, c.GetSaturation(), c.GetBrightness());
+				//Brush.Color = new Color(nextColor.R, nextColor.G, nextColor.B);
+				//Console.WriteLine(nextColor);
+
+				//sb.AppendLine(value.ToString("N2"));
+
+				xPosition += pointDistance;
+				previousValue = value;
+			}
+
+			// why stop ._.
+
+			float ValueToY(float v) // bizi sevmiyo sanırım
+			{ // paint
+				float r = (screenH - yOffset) - (v * heightMultiplier); // bidaha de ._.
+				//float r = (screenH - v * heightMultiplier) - 300;
+
+				//Console.WriteLine($"DBG = {r}");
+				
+				return r;
+
+				//return (((v * heightMultiplier) - yOffset) + screenH) * -1; // ters
+			}
+
+			float[] Normalize(float[] values, float by = 1)
+			{
+				float ratio = values.Max() / by;
+				return values.Select(i => i / ratio).ToArray();
+			}
+
+			int ConvColor(float f)
+			{
+				return (int)(f * 255);
+			}
+
+			// ah yes console log death
+			/*
+			int i = 0;
+			float prevX = 0;
+			float prevY = screenH;
+			foreach(float value in data)
+			{
+				float pointYPosition = value; //screenH - value;
+				gfx.DrawLine(Brush, prevY, prevX, prevX + pointDistance, pointYPosition, 5);
+				prevX += pointDistance;
+				prevY = pointYPosition;
+
+				sb.AppendLine(value.ToString("N2"));
+
+				//Console.WriteLine($"DBG = {i}");
+				i++;
+				//Thread.Sleep(1);
+
+				/*
 				float height = b * heightMultiplier;
 				float currentYPos = screenH - height;
 				gfx.DrawLine(Brush, posX, posY, posX + pointDistance, currentYPos, 5);
-				posX = posX + pointDistance;
+				posX += pointDistance;
 				posY = currentYPos;
-			}
-			Console.WriteLine(pointDistance);
+			}*/
+			//Console.WriteLine(pointDistance);
 			//Console.WriteLine($"Render called");
+
+
+
+
+
+			//gfx.DrawText(Font, Brush, 0, 0, sb.ToString());
 		}
 
 		public static void SetupCapture()
@@ -110,47 +221,29 @@ namespace DenVis
 			var soundInSource = new SoundInSource(soundIn);
 			ISampleSource source = soundInSource.ToSampleSource().AppendSource(x => new PitchShifter(x), out var _pitchShifter);
 
-			/*
-			//the SingleBlockNotificationStream is used to intercept the played samples
-			var notificationSource = new SingleBlockNotificationStream(aSampleSource);
-			//pass the intercepted samples as input data to the spectrumprovider (which will calculate a fft based on them)
+			
+			// this thing divides the data into blocks of audio data i think
+			var notificationSource = new SingleBlockNotificationStream(source);
+			// we pass every block to the fft thingy and magic
 			notificationSource.SingleBlockRead += (s, a) =>
 			{
+				fftProvider.Add(a.Left, a.Right);
+			};
 			
-			};*/
-
-			//var _source = notificationSource.ToWaveSource(16);
-			/*
-			// We need to read from our source otherwise SingleBlockRead is never called and our spectrum provider is not populated
-			byte[] buffer = new byte[_source.WaveFormat.BytesPerSecond / 2];
+			// no idea what this is but its neccesary...
+			float[] buffer = new float[source.WaveFormat.BytesPerSecond / 2];
 			soundInSource.DataAvailable += (s, aEvent) =>
 			{
 				int read;
-				while ((read = _source.Read(buffer, 0, buffer.Length)) > 0) ;
-			};*/
+				while ((read = notificationSource.Read(buffer, 0, buffer.Length)) > 0) ; // it just reads idk
+			};
 
-			soundIn.DataAvailable += dataAvailableEvent;
+			fftProvider = new FftProvider(soundInSource.WaveFormat.Channels, fftSize);
 
 			soundIn.Start();
 
 			Console.WriteLine($"SetupCapture done");
-		}
-
-		private static void dataAvailableEvent(object sender, DataAvailableEventArgs e)
-		{
-			if (e.ByteCount == 0) return;
-			//Console.WriteLine($"Got data with length {e.ByteCount}");
-			soundData = ConvertByteArrayToFloat(e.Data);
-			pointDistance = e.ByteCount / screenW;
-		}
-
-		public static float[] ConvertByteArrayToFloat(byte[] bytes)
-		{
-			if (bytes == null) throw new ArgumentNullException("bytes");
-			if (bytes.Length % 4 != 0) throw new ArgumentException("bytes does not represent a sequence of floats");
-			return Enumerable.Range(0, bytes.Length / 4)
-							 .Select(i => BitConverter.ToSingle(bytes, i * 4))
-							 .ToArray();
+			_initialized = true;
 		}
 	}
 }
--- DenVis/Utils.cs
+++ DenVis/Utils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Drawing;
+using System;
 using System.Runtime.InteropServices;
 
 namespace DenVis
@@ -54,6 +55,121 @@ namespace DenVis
 			EnumDisplaySettings(null, ENUM_CURRENT_SETTINGS, ref devMode);
 
 			return (devMode.dmPelsWidth, devMode.dmPelsHeight);
+		}
+
+
+
+
+		/*
+
+		public static Color SetHue(Color oldColor)
+		{
+			var temp = new HSV();
+			temp.h = oldColor.GetHue();
+			temp.s = oldColor.GetSaturation();
+			temp.v = getBrightness(oldColor);
+			return ColorFromHSL(temp);
+		}
+
+		// color brightness as perceived:
+		public static float getBrightness(Color c)
+		{ return (c.R * 0.299f + c.G * 0.587f + c.B * 0.114f) / 256f; }
+
+		// A common triple float struct for both HSL & HSV
+		// Actually this should be immutable and have a nice constructor!!
+		public struct HSV { public float h; public float s; public float v; }
+
+		// the Color Converter
+		static public Color ColorFromHSL(HSV hsl)
+		{
+			if (hsl.s == 0)
+			{ int L = (int)hsl.v; return Color.FromArgb(255, L, L, L); }
+
+			double min, max, h;
+			h = hsl.h / 360d;
+
+			max = hsl.v < 0.5d ? hsl.v * (1 + hsl.s) : (hsl.v + hsl.s) - (hsl.v * hsl.s);
+			min = (hsl.v * 2d) - max;
+			
+			Color c = Color.FromArgb(255, (int)(255 * RGBChannelFromHue(min, max, h + 1 / 3d)),
+										  (int)(255 * RGBChannelFromHue(min, max, h)),
+										  (int)(255 * RGBChannelFromHue(min, max, h - 1 / 3d)));
+			return c;
+		}
+
+		static double RGBChannelFromHue(double m1, double m2, double h)
+		{
+			h = (h + 1d) % 1d;
+			if (h < 0) h += 1;
+			if (h * 6 < 1) return m1 + (m2 - m1) * 6 * h;
+			else if (h * 2 < 1) return m2;
+			else if (h * 3 < 2) return m1 + (m2 - m1) * 6 * (2d / 3d - h);
+			else return m1;
+
+		}
+
+		// big brain but i dont want to lmao
+		public static float OldHue;
+		public static (float, float, float) AddHueToColor(float r, float b, float g)
+		{
+			Color c = Color.FromArgb(255, (int)r, (int)g, (int)b);
+
+			Color ret = ColorFromHSL(new HSV()
+			{
+				h = ((c.GetHue()) + 1) % 100, // hue 100% değilmi
+				s = c.GetSaturation(),
+				v = c.GetBrightness(),
+			});
+
+			return ((float)ret.R, (float)ret.G, (float)ret.B);
+		}*/
+	}
+
+
+
+	public static class ColorScale
+	{
+		public static Color ColorFromHSL(double h, double s, double l)
+		{
+			double r = 0, g = 0, b = 0;
+			if (l != 0)
+			{
+				if (s == 0)
+					r = g = b = l;
+				else
+				{
+					double temp2;
+					if (l < 0.5)
+						temp2 = l * (1.0 + s);
+					else
+						temp2 = l + s - (l * s);
+
+					double temp1 = 2.0 * l - temp2;
+
+					r = GetColorComponent(temp1, temp2, h + 1.0 / 3.0);
+					g = GetColorComponent(temp1, temp2, h);
+					b = GetColorComponent(temp1, temp2, h - 1.0 / 3.0);
+				}
+			}
+			return Color.FromArgb((int)(255 * r), (int)(255 * g), (int)(255 * b));
+
+		}
+
+		private static double GetColorComponent(double temp1, double temp2, double temp3)
+		{
+			if (temp3 < 0.0)
+				temp3 += 1.0;
+			else if (temp3 > 1.0)
+				temp3 -= 1.0;
+
+			if (temp3 < 1.0 / 6.0)
+				return temp1 + (temp2 - temp1) * 6.0 * temp3;
+			else if (temp3 < 0.5)
+				return temp2;
+			else if (temp3 < 2.0 / 3.0)
+				return temp1 + ((temp2 - temp1) * ((2.0 / 3.0) - temp3) * 6.0);
+			else
+				return temp1;
 		}
 	}
 }
